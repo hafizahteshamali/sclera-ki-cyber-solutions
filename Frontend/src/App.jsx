8ab1dad9a5933e44c2537af9364c3453b865d6c2
hafizahteshamali <hafizahteshamali363617@gmail.com>
2025-10-23 19:08:51 +0500

    update routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import AppRoutes from './Routes/AppRoutes'
 const App = () => {
   return (
     <>
-     <BrowserRouter basename="/predictive-maintainance">
+     <BrowserRouter basename="/predictive-maintainance/">
       <AppRoutes />
      </BrowserRouter> 
     </>
